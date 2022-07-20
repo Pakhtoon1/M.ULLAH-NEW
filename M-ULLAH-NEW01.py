57aceba4854e8a399e480d89e5bf3accbb8ff5dd
Pakhtoon1 <108829433+Pakhtoon1@users.noreply.github.com>
2022-07-20 12:13:19 +0500

    Update M-ULLAH-NEW01.py

--- M-ULLAH-NEW01.py
+++ M-ULLAH-NEW01.py
@@ -46,7 +46,7 @@ princp=[]
 
 try:
 
-	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text	open('.prox.txt','w').write(prox)
+
 
 except Exception as e:
 
